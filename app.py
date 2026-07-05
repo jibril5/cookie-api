dc215161ce52cb845271c2cad58b243dbf8cb621
jibril5 <dahmani.jibril@gmail.com>
2026-07-05 19:56:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -40,20 +40,18 @@ def test_chrome():
         chrome_options = Options()
         chrome_options.binary_location = "/usr/bin/chromium"
 
-        chrome_options.add_argument("--headless=new")
+        chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-setuid-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--disable-software-rasterizer")
         chrome_options.add_argument("--window-size=1920,1080")
-        chrome_options.add_argument("--remote-debugging-port=9222")
         chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
 
-        service = Service("/usr/bin/chromedriver")
+        driver = webdriver.Chrome(options=chrome_options)
 
-        driver = webdriver.Chrome(service=service, options=chrome_options)
         driver.get("https://example.com")
-
         return text_response(driver.title)
 
     except Exception as e:
